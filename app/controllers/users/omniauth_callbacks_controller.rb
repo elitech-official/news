fd0f9a5f153d1ee0e3833d5521e2fee4d207b17a
elitech-official <completelydifferentuser@gmail.com>
2015-12-18 09:37:30 +0200

    123

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
-    user = User.find_for_facebook_oauth request.env["omniauth.auth"]
-    if user.persisted?
+    @user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+    if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect user, :event => :authentication
+      sign_in_and_redirect root_path, :event => :authentication
     else
       flash[:notice] = "authentication error"
       redirect_to root_path
@@ -11,13 +11,18 @@ class Users::OmniauthCallbacksController < ApplicationController
   end
 
   def vkontakte
-    user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
-    if user.persisted?
+    @user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
+    if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
-      sign_in_and_redirect user, :event => :authentication
+      sign_in_and_redirect root_path, :event => :authentication
     else
       flash[:notice] = "authentication error"
       redirect_to root_path
     end
   end
+  
+  def user_params
+      params.require(:user).permit(:username, :nickname, :provider, :url)
+    end
+  
 end
