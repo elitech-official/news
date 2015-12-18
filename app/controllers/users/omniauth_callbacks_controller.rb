72f81ed0fd7a4afcacd44cfe8ef5263d9677f06f
elitech-official <completelydifferentuser@gmail.com>
2015-12-18 08:54:40 +0200

    hh

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,23 +1,23 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def facebook
-    <hh user=user> = User.find_for_facebook_oauth request.env["omniauth.auth"]
-    if <hh user=user>.persisted?
+    user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+    if user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect <hh user=user>, :event => :authentication
-    else
-      flash[:notice] = "authentication error"
-      redirect_to root_path
-  end
-  
-  def vkontakte
-    <hh user=user> = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
-    if <hh user=user>.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
-      sign_in_and_redirect <hh user=user>, :event => :authentication
+      sign_in_and_redirect user, :event => :authentication
     else
       flash[:notice] = "authentication error"
       redirect_to root_path
     end
   end
-  
+
+  def vkontakte
+    user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
+    if user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
+      sign_in_and_redirect user, :event => :authentication
+    else
+      flash[:notice] = "authentication error"
+      redirect_to root_path
+    end
+  end
 end
